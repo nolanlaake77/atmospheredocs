b83060071ed9bc533b4df61687110eae25d55958
Nolan <iamchunkerdunkers@gmail.com>
2023-10-18 10:53:48 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'docs.atmospheresupport.online',
+  url: 'https://docs.atmospheresupport.online',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
